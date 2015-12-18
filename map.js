58abca576ba0e37edbe3b3ecc50c869110fb6866
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-17 20:03:46 -0500

    moved mapping stuff to a seperate JS file

---
+++ map.js
@@ -0,0 +1,19 @@
+
+var map = L.map('map').setView([42.35, -71.05], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets'
+    }).addTo(map);
+
+
+
+function onMapClick(e) {
+popup
+.setLatLng(e.latlng)
+.setContent("You clicked the map at " + e.latlng.toString())
+.openOn(map);
+}
+map.on('click', onMapClick);
