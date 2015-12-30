da15f809766052cf1ef3f9922e67da7f32f27bd7
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-29 21:31:37 -0500

    removed some boilerplate

--- map.js
+++ map.js
@@ -8,16 +8,6 @@ var mapOptions = {
 var map = L.map('map').setView([42.35, -71.05], 13);
 L.tileLayer(tileLayer, mapOptions).addTo(map);
 
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
-//     maxZoom: 18,
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-//     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-//     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//     id: 'mapbox.streets'
-//     }).addTo(map);
-// var layer = L.tileLayer(, {
-// });
-
 var marker = L.marker([51.5, -0.09]).addTo(map);
 var markers = []
 
