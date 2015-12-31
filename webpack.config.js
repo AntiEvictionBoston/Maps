103b8c49890da2e7ecfc66e4e7632cb683ec8206
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-31 10:26:28 -0500

    boilerplate webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  path = require('path'),
+  srcPath = path.join(__dirname, 'src');
+
+module.exports = {
+  target: 'web',
+  cache: true,
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    path.join(srcPath, 'map.js')
+  ],
+  resolve: {
+    root: srcPath,
+    extensions: ['', '.js', '.jsx'],
+    modulesDirectories: ['node_modules', 'src']
+  },
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader']},
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader']},
+      { test: /\.css$/, loader: "style!css" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.html'
+    }),
+    new webpack.NoErrorsPlugin()
+  ],
+  debug: true,
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    historyApiFallback: true
+  }
+};
