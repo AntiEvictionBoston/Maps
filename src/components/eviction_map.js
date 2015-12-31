132c161774420cd01efae7394fd9d60269a974c7
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-31 16:20:41 -0500

    es6 + react is a little tricky

--- src/components/eviction_map.js
+++ src/components/eviction_map.js
@@ -3,14 +3,18 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 class EvictionMap extends React.Component {
   constructor(options) {
-    super();
-    this.position = options.loc;
-    this.zoom = options.zoom;
+    super(options);
+  }
+
+  static propTypes = {
+    position:     React.PropTypes.array.isRequired,
+    zoom:         React.PropTypes.number.isRequired,
+    evictions:    React.PropTypes.array.isRequired
   }
 
   render() {
     return (
-      <Map center={this.position} zoom={this.zoom}>
+      <Map center={this.props.position} zoom={this.props.zoom}>
         <TileLayer
           url="http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
@@ -19,6 +23,10 @@ class EvictionMap extends React.Component {
     );
   }
 
+  renderEvictions () {
+    this.props.evictions
+
+  }
 }
 
 export default EvictionMap;
