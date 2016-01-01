d9ddf0beddb5282fd3e60848d2f168279241cb96
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-01 10:05:05 -0500

    re-complicated the script

--- src/map.js
+++ src/map.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import EvictionMap from "components/eviction_map";
 import eastBostonEvictions from "data/east_boston_evictions";
 
+
 render((
   <EvictionMap
     position={[42.37, -71.03]}
