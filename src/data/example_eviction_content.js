59f60ca9edc40deb7d1774c4b471ccf164333600
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-04 20:04:25 -0500

    some example content to show on eviction pins

---
+++ src/data/example_eviction_content.js
@@ -0,0 +1,18 @@
+const evictionContent = {
+  imageURL: "https://scontent-lga3-1.xx.fbcdn.net/hphotos-xfp1/t31.0-0/p180x540/11889982_10153539563424161_8634583050576839373_o.jpg",
+  videoURL: "https://www.youtube.com/watch?v=uo6IXuESXYU&list=PLStYO1F2VYmR-JBl2La3LZLgULEDD4V80&index=8",
+  narrative: `Before the dramatic wall collapse of 173, 175 and 177 Maverick St. on
+Saturday which displaced 30 East Boston residents, if you walked into
+Hoober Alvarez’ apartment and looked up, you would be able to see the
+upstairs neighbor’s floor through a 5 foot long hole in the ceiling. In
+this same building, families went without heat, water or electricity during
+the coldest months of last winter without response from their landlord.
+
+However, when the landlord decided to give rent increases to all of the
+tenants in these buildings (in one case, as high as a $1,000 increase),
+residents had had enough. They used their legal rights to fight for better
+building conditions and to try to negotiate a long-term lease with
+reasonable rent increases.
+`};
+
+export default evictionContent;
