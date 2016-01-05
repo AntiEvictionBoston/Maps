6590281db5648af4d1264849c189ac615cd872a1
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-04 20:05:08 -0500

    add the content to each eviction

--- src/map.js
+++ src/map.js
@@ -2,6 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import EvictionMap from "components/eviction_map";
 import eastBostonEvictions from "data/east_boston_evictions";
+import exampleEvictionContent from "data/example_eviction_content";
+
+for (var eviction of eastBostonEvictions) {
+  eviction.content = exampleEvictionContent;
+}
 
 render((
   <EvictionMap
