d569b92da465dbc478a979ef5a72cbeeefdc5dd5
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-07 18:59:52 -0500

    information about tenant associations in East Boston

---
+++ src/data/east_boston_tenant_associations.js
@@ -0,0 +1,47 @@
+var tenantAssociations = [
+  { address: "173-177 Maverick St.",
+    owner: "Maverick Street Realty",
+    units: 7
+  },
+  {
+    address: "142 Gove St.",
+    owner: "RJ Leyden LLC",
+    units: 5
+  },
+  {
+    address: "64 Lubec St.",
+    owner: "Hodara",
+    units: 4
+  },
+  {
+    address: "219 Maverick St.",
+    owner: "Hodara", 
+    units: 2
+  },
+  {
+    address: "220 Saratoga St.",
+    owner: "Hodara",
+    units: 1
+  },
+  {
+    address: "25 Chelsea St.",
+    owner: "Newman",
+    units: 3
+  },
+  {
+    address: "1022 Bennington St.",
+    owner: "EB Ventures",
+    units: 4
+  },
+  { address: "354 Chelsea St.",
+    owner: "Concepcion Perez",
+    units: 2
+  },
+  {
+    address: "107-11 Brooks St.",
+    owner: "David Gradus",
+    units: 1
+  }
+]
+
+export default tenantAssociations;
