6f20c7e2006e7119e0b4ad45baf5e73a2af0408f
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-07 19:07:07 -0500

    store maps in variables? why not?

--- src/map.js
+++ src/map.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { render } from "react-dom";
 import EvictionMap from "components/eviction_map";
+import TenantAssociationMap from "components/tenant_association_map";
 import eastBostonEvictions from "data/east_boston_evictions";
 import exampleEvictionContent from "data/example_eviction_content";
+import tenantAssociations from "data/east_boston_tenant_associations";
 
 for (var eviction of eastBostonEvictions) {
   eviction.content = exampleEvictionContent;
 }
 
-render((
+let basicFakeDataMap =
   <EvictionMap
     position={[42.37, -71.03]}
     zoom={14}
-    evictions={eastBostonEvictions} />), document.getElementById('map'));
+    evictions={eastBostonEvictions} />
+
+let tenantAssociationMap =
+  <TenantAssociationMap
+    position={[42.37, -71.03]}
+    zoom={14}
+    associations={tenantAssociations} />
+
+render(basicFakeDataMap, document.getElementById('map'));
+
+// render((
+//   <EvictionMap
+//     position={[42.37, -71.03]}
+//     zoom={14}
+//     evictions={eastBostonEvictions} />), document.getElementById('map'));
