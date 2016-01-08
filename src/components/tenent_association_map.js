4d80644650a4663eeb981c55e20b9ecd3c8999af
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-07 19:10:31 -0500

    copypasta from the other map

---
+++ src/components/tenent_association_map.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import EvictionPopup from "components/eviction_popup";
+
+class TenantAssociationMap extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static propTypes = {
+    position:     React.PropTypes.array.isRequired,
+    zoom:         React.PropTypes.number.isRequired,
+    associations: React.PropTypes.array.isRequired
+  }
+
+  render() {
+    return (
+      <Map center={this.props.position} zoom={this.props.zoom}>
+        <TileLayer
+          url="http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+        />
+        {this.renderEvictions()}
+      </Map>
+    );
+  }
+
+  renderEvictions () {
+    var markers = []
+    this.props.evictions.forEach( (eviction, index) => (
+      markers.push(
+        <Marker
+          map={this.props.map}
+          position={[eviction.Latitude, eviction.Longitude]}
+          key={index}>
+          <Popup>
+            <EvictionPopup eviction={eviction}/>
+          </Popup>
+        </Marker>
+      )));
+    return markers
+  }
+}
+
+export default EvictionMap;
+
