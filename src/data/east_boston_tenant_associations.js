b4162d2935fa670329b83dbc2bd1980ea560c461
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-07 19:28:37 -0500

    added hastily sourced lat/long from google maps

--- src/data/east_boston_tenant_associations.js
+++ src/data/east_boston_tenant_associations.js
@@ -1,46 +1,64 @@
 var tenantAssociations = [
   { address: "173-177 Maverick St.",
     owner: "Maverick Street Realty",
-    units: 7
+    units: 7,
+    Latitude: 42.369521,
+    Longitude: -71.037244
   },
   {
     address: "142 Gove St.",
     owner: "RJ Leyden LLC",
-    units: 5
+    units: 5,
+    Latitude: 42.370093,
+    Longitude: -71.033843
   },
   {
     address: "64 Lubec St.",
     owner: "Hodara",
-    units: 4
+    units: 4,
+    Latitude: 42.370724,
+    Longitude: -71.033808
   },
   {
     address: "219 Maverick St.",
-    owner: "Hodara", 
-    units: 2
+    owner: "Hodara",
+    units: 2,
+    Latitude: 42.368669,
+    Longitude: -71.035611
   },
   {
     address: "220 Saratoga St.",
     owner: "Hodara",
-    units: 1
+    units: 1,
+    Latitude: 42.378119,
+    Longitude: -71.033425
   },
   {
     address: "25 Chelsea St.",
     owner: "Newman",
-    units: 3
+    units: 3,
+    Latitude: 42.370697,
+    Longitude: -71.037756
   },
   {
     address: "1022 Bennington St.",
     owner: "EB Ventures",
-    units: 4
+    units: 4,
+    Latitude: 42.387102,
+    Longitude: -71.006143
   },
   { address: "354 Chelsea St.",
     owner: "Concepcion Perez",
-    units: 2
+    units: 2,
+    Latitude: 42.378097,
+    Longitude: -71.029607
   },
   {
     address: "107-11 Brooks St.",
     owner: "David Gradus",
-    units: 1
+    units: 1,
+    Latitude: 42.377834,
+    Longitude: -71.034292
   }
 ]
 
