ba0cd2f46c0ed60aad6a21eb1a06919a5e4e05dc
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-10 13:08:03 -0500

    things are a little more set-up

--- src/components/tenant_association_popup.js
+++ src/components/tenant_association_popup.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 class TenantPopup extends React.Component {
+  static propTypes = {
+    tenantAssociation: React.PropTypes.object.isRequired
+  }
+
+  render () {
+    return (
+    );
+  }
 }
 
 export default TenantPopup;
