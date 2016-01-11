62b7a3bcf7d5289133cb41ebfb9ba999cb858919
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-10 21:37:17 -0500

    a thing that exports the images

---
+++ src/images/images.js
@@ -0,0 +1,15 @@
+const photos = {
+  benningtonSt: require("file!./1022_bennington_st.png"),
+  brooksSt: require("file!./107-11_brooks_st.png"),
+  maverickStreet219: require("file!./219_maverick_st.png"),
+  maverickStreet173: require("file!./173_177_maverick_st.png"),
+  saratogaPorch: require("file!./220_saratoga_porch.png"),
+  saratogaStreet: require("file!./220_saratoga_street.png"),
+  chelseaStreet25: require("file!./25_chelsea_st.png"),
+  chelseaStreet354: require("file!./354_chelsea_street.png"),
+  lubecStreet: require("file!./64_lubec_street.png"),
+  goveStreetMailbox: require("file!./gove_street_mailboxes.png"),
+  goveStreet: require("file!./gove_street.png")
+}
+
+export default photos;
