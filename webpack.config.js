030b3358daadffc2005f837b72eb4e10e0e578ee
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-11 08:03:41 -0500

    package up the images properly

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
     loaders: [
       {test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader']},
       {test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader']},
-      { test: /\.css$/, loader: "style!css" }
+      {test: /\.css$/, loader: "style!css" },
+      {test: /\.(png|jpg)$/, loaders: ['url-loader?limit=8192', 'img']}
     ]
   },
   plugins: [
