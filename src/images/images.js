078b133821c925a051ced081642068e61b77cf10
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-17 16:54:24 -0500

    we're sourcing images from the utility repo

--- src/images/images.js
+++ src/images/images.js
@@ -1,15 +1,15 @@
 const photos = {
-  benningtonSt: require("./1022_bennington_st.png"),
-  brooksSt: require("./107-11_brooks_st.png"),
-  maverickStreet219: require("./219_maverick_st.png"),
-  maverickStreet173: require("./173_177_maverick_st.png"),
-  saratogaPorch: require("./220_saratoga_porch.png"),
-  saratogaStreet: require("./220_saratoga_street.png"),
-  chelseaStreet25: require("./25_chelsea_st.png"),
-  chelseaStreet354: require("./354_chelsea_street.png"),
-  lubecStreet: require("./64_lubec_street.png"),
-  goveStreetMailbox: require("./gove_street_mailboxes.png"),
-  goveStreet: require("./gove_street.png")
+  benningtonSt: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/1022_bennington_st.png",
+  brooksSt: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/107-11_brooks_st.png",
+  maverickStreet219: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/219_maverick_st.png",
+  maverickStreet173: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/173_177_maverick_st.png",
+  saratogaPorch: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/220_saratoga_porch.png",
+  saratogaStreet: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/220_saratoga_street.png",
+  chelseaStreet25: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/25_chelsea_st.png",
+  chelseaStreet354: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/354_chelsea_street.png",
+  lubecStreet: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/64_lubec_street.png",
+  goveStreetMailbox: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/gove_street_mailboxes.png",
+  goveStreet: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/gove_street.png"
 }
 
 export default photos;
