a70fdebaab5426682921230436a9f972a8eddc98
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-17 20:42:47 -0500

    add a test for *.scss, chain sass -> css -> style

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ module.exports = {
       {test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader']},
       {test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader']},
       {test: /\.css$/, loader: "style!css" },
-      {test: /\.(png|jpg)$/, loaders: ['url-loader?limit=8192', 'img']}
+      {test: /\.(png|jpg)$/, loaders: ['url-loader?limit=8192', 'img']},
+      { test: /\.scss$/, loader: "style!css!sass" }
     ]
   },
   plugins: [
