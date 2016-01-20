942d53ee4ed9bc1abc4904c5c2efab4ba8a1dc12
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-20 07:50:12 -0500

    little refactor, pull in the mapStore to map.js

--- src/map.js
+++ src/map.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 import EvictionMap from "components/eviction_map";
 import TenantAssociationMap from "components/tenant_association_map";
 import eastBostonEvictions from "data/east_boston_evictions";
 import exampleEvictionContent from "data/example_eviction_content";
 import tenantAssociations from "data/east_boston_tenant_associations";
 import mapStore from "./stores/map_store";
+import * as actions from "./actions/actions";
 require("./stylesheets/main.scss");
+
+let store = mapStore;
 
 // check for different divs, render the appropriate map
 if (document.getElementById('east_boston_tenant_association_map')) {
-  let tenantAssociationMap =
-    <TenantAssociationMap
-      position={[42.37, -71.03]}
-      zoom={14}
-      associations={tenantAssociations} />
-  render(tenantAssociationMap, document.getElementById('east_boston_tenant_association_map'));
+  let domElement = document.getElementById('east_boston_tenant_association_map');
+  render(
+    <Provider store={store}>
+      <TenantAssociationMap
+        position={[42.37, -71.03]}
+        zoom={14}
+        store={store}
+        associations={tenantAssociations} />
+    </Provider>,
+    domElement
+  );
 }
 else if (document.getElementById('fake_data_map')) {
   for (var eviction of eastBostonEvictions) {
