a701c545fd452be08fa0ff4d8b40aebc71c12e68
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-31 16:57:23 -0500

    add the letter for 142 gove st

--- src/data/east_boston_tenant_associations.js
+++ src/data/east_boston_tenant_associations.js
@@ -21,7 +21,33 @@ var tenantAssociations = [
     images: [
       photos.goveStreetMailbox,
       photos.goveStreet
-    ]
+    ],
+    tenantAssociationLetter: `142 Gove Street Tenant Association
+c/o City Life/ Vida Urbana 28 Paris St. Boston, MA 02128
+
+Dear Landlord of 142 Gove Street,						September 30,2014
+We wanted to inform you that on September 27th, 2014 we formed the 142 Gove Street Tenant Association. Our goal is to preserve the affordability and safety of our homes. We are seeking to come to agreeable terms regarding the issues below:
+	Rent Increases: Firstly, all of our members received large rent increases for the second time in less than 6 months. The second increase was of up to $150 for some units in the building. Although we know this building has increased in value, we believe that the increase is largely the result of the investment we have made, as residents, to improve this neighborhood. For these reasons, we would like to negotiate no future rent increases. We want an agreement in which the tenants' share of the rent remains as close to 32% of income as possible. We would like the security for our children and family of knowing we will not be at the whim of changing decisions. Rather, that terms be agreed upon so that we may be fiscally and otherwise responsible for our household matters. 
+	Our children attend local schools including the one just across the street, and some have very strong medical needs that require significant attention, time, and expenses. It is for these reasons that we have invested what for some is upwards of ten years in this building and community. We feel strongly that you should take that into account. While we understand that many investors are wanting to purchase in our neighborhood, we would also like for those like yourselves to understand that this is not vacant land. It has residents and families that have strong roots in this community. We appreciate your investment into our neighborhood and would like to see you as a partner in continuing the work to build a strong community. 
+	Illegal Entry: Secondly, we are very concerned that your employees and/or representatives/ contractors have repeatedly attempted to enter our units without permission. Per Massachusetts landlord-tenant law, it is illegal for anyone to enter a tenant's unit without the required notice. Any further entry without permission will be treated by us as illegal trespassing.	
+	Condition Problems of Units: Thirdly, many repairs need to be made to ensure the safety of our apartments. It is your responsibility, as the landlord, to maintain the livability of our units.  The problems include, but are not limited to: 
+
+evidence of mold
+broken screen doors
+problems with the heat
+collapsing ceilings
+problems with hot water
+rat infestation
+roach infestation
+bedbugs
+
+broken or missing floorboards
+We understand that as owners you may also have concerns that you wish to raise with us. We think such discussions of both sets of issues could lead to balance between your desire for profit and our concern about the safety and affordability of our homes. We would be willing to make use of a City mediator. They are scrupulously neutral and could assist in discussions.
+Thank you for your attention to this matter.
+
+Sincerely,
+
+cc’d: Sheila A. Dillon, Chief and Director Department of Neighborhood Development; Salvatore Lamattina, DIstrict 1 City Councillor; Elizabeth Warren, U.S Senator for Massachusetts; Carlos Basile, City Speaker`
   },
   {
     address: "64 Lubec St.",
