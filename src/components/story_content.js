141b39adaebab3f244ef66c30967b405fb08f05c
Alice Pote <alice.writes.wrongs@gmail.com>
2016-02-14 16:23:05 -0500

    remove the accordion component from the StoryContent component

--- src/components/story_content.js
+++ src/components/story_content.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import RS from "react-sanfona";
 import StoryMedia from "./story_media";
 
 class StoryContent extends React.Component {
   render () {
     return (
       <div id="story-content">
-        <RS.Accordion>
-          <RS.AccordionItem title="Building Info">
-            Owner: {this.props.story.owner}
-          </RS.AccordionItem>
-          <RS.AccordionItem title="Media">
-            <StoryMedia {...this.props} />
-          </RS.AccordionItem>
-        </RS.Accordion>
+        Owner: {this.props.story.owner}
+        <StoryMedia {...this.props} />
       </div>
     );
   }
