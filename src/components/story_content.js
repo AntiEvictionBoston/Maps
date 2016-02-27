56b6bef55a90ae5819b5bdfdfd3afbff91cea0c6
Alice Pote <alice.writes.wrongs@gmail.com>
2016-02-27 09:48:28 -0500

    starting the 'basic info' table

--- src/components/story_content.js
+++ src/components/story_content.js
@@ -6,9 +6,35 @@ class StoryContent extends React.Component {
   render () {
     return (
       <div id="story-content">
-        Owner: {this.props.story.owner}
+        {this.basicInfo()}
+
         <StoryMedia {...this.props} />
       </div>
+    );
+  }
+
+  basicInfo () {
+    return (
+      <table id="story-basic-info">
+        <tbody>
+          <tr id="rent-row" className="row">
+            <td id="rent-col-one" className="cell">
+              Rent <i className="fa fa-arrow-up fa-2x"></i>
+            </td>
+            <td id="rent-col-two" className="cell">
+              {this.props.story.rent}
+            </td>
+          </tr>
+          <tr id="landlord-row" className="row">
+            <td id="landlord-col-one" className="cell">
+              Landlord
+            </td>
+            <td id="landlord-col-two" className="cell">
+              {this.props.story.owner}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
