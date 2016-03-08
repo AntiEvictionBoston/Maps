7487411a533be7afd5dfcf4109aba494ec4a52f6
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-08 16:15:07 -0500

    fixing strange issues with image stuff
    not sure why but one of the images wasn't being sourced correctly.
    
    pointing at the particular commit hash got around it.

--- src/images/images.js
+++ src/images/images.js
@@ -2,7 +2,7 @@ export const photos = {
   benningtonSt: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/10_bennington_st.png",
   brooksSt: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/10_brooks_st.png",
   maverickStreet21: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/21_maverick_st.png",
-  maverickStreet17: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/17_maverick_st.png",
+  maverickStreet17: "https://cdn.rawgit.com/AntiEvictionBoston/utility/7d72d309b144cd5dd25f5ce53563b33472d775eb/images/17_maverick_st.png",
   saratogaPorch: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/22_saratoga_porch.png",
   saratogaStreet: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/22_saratoga_street.png",
   chelseaStreet25: "https://cdn.rawgit.com/AntiEvictionBoston/utility/master/images/25_chelsea_st.png",
