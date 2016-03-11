2307f6143fda3016947e75ec65cd9882ff05530d
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-11 10:40:39 -0500

    using hash urls

--- src/map.js
+++ src/map.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
-import { Router, Route, browserHistory } from "react-router";
+import { Router, Route, hashHistory } from "react-router";
 
 import StoryMap from "components/story_map";
 import eastBostonStories from "./data/east_boston_stories";
@@ -22,7 +22,7 @@ if (document.getElementById('east_boston_tenant_association_map')) {
 
   render(
     <Provider store={store}>
-      <Router history={browserHistory}>
+      <Router history={hashHistory}>
         <Route path="/" component={StoryMap}>
           <Route path="/:address" />
         </Route>
