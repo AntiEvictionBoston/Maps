9da7e69caf37337605c173e2b87ce7ca191db3d9
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-12 12:34:04 -0500

    fix webpack configuration
    was doing a couple of silly things
    JS console in prod should be error-free now.

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,48 @@ var isProduction = function () {
   return process.env.NODE_ENV === 'production';
 };
 
+function getPlugins() {
+  var plugins = [];
+
+  plugins.push(new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.html'
+    }));
+  plugins.push(new webpack.NoErrorsPlugin());
+
+  if (isProduction()) { // prod plugins
+    plugins.push(new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }));
+  }
+  else { // dev plugins
+  }
+  return plugins;
+}
+
+function entryPoints () {
+  if (isProduction()) {
+    return { main: [path.join(srcPath, 'map.js')] };
+  }
+  else {
+    return [
+      'webpack-dev-server/client?http://0.0.0.0:8080',
+      'webpack/hot/only-dev-server',
+      path.join(srcPath, 'map.js')
+    ];
+  }
+}
+
+function getJSLoaders () {
+  return isProduction() ? ['babel-loader'] : ['react-hot', 'babel-loader'];
+}
+
 module.exports = {
   target: 'web',
   cache: true,
-  entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080',
-    'webpack/hot/only-dev-server',
-    path.join(srcPath, 'map.js')
-  ],
+  entry: entryPoints(),
   resolve: {
     root: srcPath,
     extensions: ['', '.js', '.jsx'],
@@ -28,19 +62,13 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader']},
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader']},
+      {test: /\.jsx$/, exclude: /node_modules/, loaders: getJSLoaders()},
+      {test: /\.js$/, exclude: /node_modules/, loaders: getJSLoaders()},
       {test: /\.scss$/, loader: "style!css!sass" },
       { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/, loader: "file" }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: 'src/index.html'
-    }),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins: getPlugins(),
   debug: isProduction() ? false : true,
   devtool: isProduction() ? '' : 'eval-cheap-module-source-map',
   devServer: {
