6754430d4a7213eac9fa36366df4b9d6d0a474da
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-13 12:17:12 -0400

    creating a wrapper component for facebook videos

---
+++ src/components/facebook_video.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+
+class FacebookVideo extends React.Component {
+  static propTypes = {
+    url:    React.PropTypes.string.isRequired
+  };
+
+  render () {
+  }
+}
+
+export default FacebookVideo;
