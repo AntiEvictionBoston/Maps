5dc710567f40cb444fce2f7fae478aecfb127e91
Benji Mauer <benji@heybenji.co>
2016-03-14 02:46:45 -0400

    all the data from the master list

--- src/data/east_boston_stories.js
+++ src/data/east_boston_stories.js
@@ -1,6 +1,18 @@
 import Images from "../images/images";
 
 let eastBostonStories = {
+  "chelsea_st_1": {
+    title: "Marta, Ruth, and Margarita",
+    owner: "Newman Properties",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "$500+",
+    video: "https://www.facebook.com/bostondisplacement/videos/186057325103944/",
+    story: `All of the tenants in this building received a no-fault eviction notice. Marta, one of the renters, says, "When I tried to get in touch with the new owners, they told me it was because the tenants of this building couldn't manage to pay between $1800 and $2100 a month - I pay $1300 a month - and it was for that reason that we were being evicted." Marta adds, "We're always talking with the people in our community about what's going on in East Boston, that they should push for their rights, that they should know that we have rights too."`,
+    Latitude: 42.370697,
+    Longitude: -71.037756,
+    images: [],
+  },
   "lubec_st": {
     title: "Ana and Luis",
     owner: "Hodara",
@@ -11,9 +23,10 @@ let eastBostonStories = {
     story: `When Alex Hodara bought the building Ana and Luis live in, he served no-fault eviction notices to the tenants the next month. "We didn't get an exact explanation for why they had to evict us," says Luis, whose child with special needs attends a nearby school. Seven tenants in this building signed a letter to Hodara demanding a "just cause" for eviction.`,
     Latitude: 42.370336,
     Longitude: -71.033936,
+    images: [],
   },
   "maverick_st_1": {
-    title: "Maverick Street Tenants",
+    title: "Maverick Street Families",
     owner: "Maverick Street Realty",
     type: "story",
     center: [42.37, -71.03],
@@ -22,6 +35,7 @@ let eastBostonStories = {
     story: `Thirty-one tenants, including 8 children, were displaced when the back wall of their adjacent buildings partially collapsed (shortly prior to the collapse, they had received a rent increase). After months of living in hotels, several families took refuge in the basement of Our Saviour's Lutheran Church nearby. One family continues to live in the church 7 months after the collapse. The owner would not rehabilitate the apartments for the tenants who lived there.`,
     Latitude: 42.369521,
     Longitude: -71.037244,
+    images: [],
   },
   "gove_st": {
     title: "Natalia",
@@ -33,6 +47,7 @@ let eastBostonStories = {
     story: `Natalia, one of the tenants in this building, has lived in East Boston for 15 years and has lived on Gove St. for about 14 of those years. First, Natalia and the other tenants here received rent increases - Natalia's increase was $600 a month. After that, "We received a 14-day eviction notice," says Natalia. The owner apparently wanted to "clear out" the building.`,
     Latitude: 42.370093,
     Longitude: -71.033843,
+    images: [],
   },
   "maverick_st_2": {
     title: "Idalia",
@@ -44,17 +59,7 @@ let eastBostonStories = {
     story: `When Alex Hodara bought this building, he gave no-fault eviction notices to all the tenants. At first, Idalia didn't fully understand the eviction notice, written only in English. So, she brought it to City Hall, and they told her it meant that she had 30 days to leave her house.`,
     Latitude: 42.368669,
     Longitude: -71.035611,
-  },
-  "chelsea_st_1": {
-    title: "Marta, Ruth, and Margarita",
-    owner: "Newman Properties",
-    type: "story",
-    center: [42.37, -71.03],
-    rent: "$500+",
-    video: "https://www.facebook.com/bostondisplacement/videos/186057325103944/",
-    story: `All of the tenants in this building received a no-fault eviction notice. Marta, one of the renters, says, "When I tried to get in touch with the new owners, they told me it was because the tenants of this building couldn't manage to pay between $1800 and $2100 a month - I pay $1300 a month - and it was for that reason that we were being evicted." Marta adds, "We're always talking with the people in our community about what's going on in East Boston, that they should push for their rights, that they should know that we have rights too."`,
-    Latitude: 42.370697,
-    Longitude: -71.037756,
+    images: [],
   },
   "princeton_st_1": {
     title: "Alma and Obdulio",
@@ -66,6 +71,7 @@ let eastBostonStories = {
     story: `Alma and Obdulio have two young children. They moved to Boston from Mexico about 10 years ago. They've lived in their current apartment less than half of that time. Their landlord, who owns several properties in East Boston, gave them a rent increase of $250 per month and later a 30-day eviction notice. "This case is definitely part of a bigger trend in East Boston of the displacement of families, especially lower-income families, immigrant families," says Hannah, Alma and Obdulio's attorney.`,
     Latitude: 42.376574,
     Longitude: -71.038111,
+    images: [],
   },
   "ruggles_st_1": {
     title: "Felicha, Purple, and Alzina",
@@ -80,7 +86,170 @@ let eastBostonStories = {
       Images.images.rugglesstreet9131
     ],
   },
+  "school_st_1": {
+    title: "Dora, Rene, and Dulce",
+    owner: "N/A",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "N/A",
+    video: "https://www.facebook.com/bostondisplacement/videos/186792875030389/",
+    story: `All of the tenants in this building received no-fault eviction notices. Several children and teenagers live there, like Dulce who attends Boston Latin School. They're worried that the eviction will disrupt their eduction and work, because rising rents in Boston prevent them from finding a new home within the city.`,
+    Latitude: 42.313222,  
+    Longitude: -71.097608,
+    images: [],
+  },
+  "chilcott_1": {
+    title: "Josue",
+    owner: "N/A",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "N/A",
+    story: `Josue grew up in a house in Egleston Square. But after 17 years, Josue's family was displaced out of the neighborhood when their rent went up. Josue says, "The whole entire street was awesome, we all got along, especially in a snowstorm...everybody knew each other. But then as soon as the landlord decided to raise up the bills, people were leaving left and right." Josue still prefers his old neighborhood, and he tries to spend time with his friends and community in Egleston even since he's been displaced. Josue works with teens at the YMCA and engages them in local issues.`,
+    Latitude: 42.313459,  
+    Longitude: -71.099741,
+    images: [
+      Images.images.chilcott11
+    ],
+  },
+  "lesher_st_1": {
+    title: "Lesher Street Families",
+    owner: "HHC One Arnold LLC",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "$300",
+    video: "https://www.facebook.com/bostondisplacement/videos/187214788321531/",
+    story: `This building is home to 5 single parents and their kids. They received rent increases of $300 per month per unit. Then they received no-fault eviction notices. They've united in the face of their landlord's attempt to "clear-out" their building. They have built community watching each other's children and supporting each other while their housing is unstable. `,
+    Latitude: 42.292612,  
+    Longitude: -71.122229,
+    images: [],
+  },
+  "river_st_1": {
+    title: "Canute",
+    owner: "HHC One Arnold LLC",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "$750",
+    video: "https://www.facebook.com/bostondisplacement/videos/187214788321531/",
+    story: `Canute has lived for over a decade in an apartment building in Mattapan managed by a corporation called Advanced Property Management (APM). In several buildings in the area, APM has hundreds of tenants. Scores of tenants throughout these buildings have recently received rent increases in the range of $150 to $750 per month. "I got a notice stating that [my landord] Advanced Property Management needs $750 more per month. Now, I'm on a fixed income—I can't afford that, there's no way I can ... If I disagree with what they said, they will proceed in an eviction notice. I'm 72 years old, and I don't see myself being homeless. At my age, where am I going to go? My blood pressure is so high. ... Each morning I'm leaving my apartment and I'm looking for some kind of note attached to my door. It's very scary to me. Each day I'm so tense."`,
+    Latitude: 42.259522,  
+    Longitude: -71.117487,
+    images: [],
+  },
+  "rosa_st_1": {
+    title: "Sally and Kathleen",
+    owner: "Advanced Property Management",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "$150-$475",
+    story: `Sally has lived in her home on Rosa Street for 14 years. Advanced Property Management recently gave Sally a rent increase of $150 per month. Sally's rent has gone up incrementally over the years, but this is the largest increase yet. Sally says, "It's overwhelming. Especially when you have children that are still in school. You don't know from day to day what these people are gonna do or what they're thinking. So I think that's a lot of pressure within itself."
+    
+    Kathleen, a 9-year-long tenant in the same building as Sally, received a $475 rent increase in 2016."`,
+    Latitude: 42.261991, 
+    Longitude: -71.112153,
+    images: [
+      Images.images.rosastreet1,
+      Images.images.rosastreet2,
+    ],
+  },
+  "river_st_2": {
+    title: "Eddy",
+    owner: "Advanced Property Management",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "$500",
+    video: "https://www.facebook.com/bostondisplacement/videos/183450632031280/",
+    story: `Eddy has lived in his home on River St. Terrace for about 35 years. Eddy's 2 daughters were raised there. Advanced Property Management is asking for $500 a month more from Eddy's household. Eddy's wife recently suffered a stroke, and Eddy is retired and disabled. They live on a fixed income. "We have no where to go, because everywhere you go, rents are sky-high...We built the community - it's our community...I'm gonna stand firm, not to be removed."`,
+    Latitude: 42.260764, 
+    Longitude: -71.112528,
+    images: [],
+  },
+  "river_st_3": {
+    title: "Alan",
+    owner: "Advanced Property Management",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "$300",
+    story: `Alan is a tenant of Advanced Property Management (APM). APM has hiked the rents of scores of their residents in Hyde Park and Mattapan. Alan says, "I received a $300 per month rent increase, and they're not negotiating with us." The increase would bring his rent payment to far beyond 50% of his income.`,
+    Latitude: 42.269486, 
+    Longitude: -71.091097,
+    images: [
+      Images.images.riverstreet431,
+    ],
+  },
+  "johnny_ct_1": {
+    title: "Ms. Huang and Ms. Zhong",
+    owner: "N/A",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "$300",
+    story: `"Ms. Huang and Ms. Zhong live in separate apartments in the same building. Ms. Huang and her husband are on the top floor, and they have 3 young children, including an infant. 
+    Ms. Zhong lives with her husband and two daughters downstairs from Ms. Huang. In 2015, their row-house style building was sold to a new owner for over 1 million dollars. 
+    
+    The landlord raised Ms. Huang's rent in 2015 by $300 a month, and in 2016 is seeking to raise the rent again by another $250. Ms. Huang asked the landlord to lower the rate of rent increases, because her family is now paying more than 50% of their income in rent.
+    
+    Ms. Zhong's rent has risen by several hundred dollars to a total of $2600 per month. Because Ms. Zhong's family can't afford $2600 per month, and they're unable to find an affordable alternative in Chinatown, they're likely to be displaced from Chinatown in the spring of 2016."`,
+    Latitude: 42.347993,
+    Longitude: -71.063258,
+    images: [
+      Images.images.johnny11,
+      Images.images.johnny12,
+    ],
+  },
+  "beach_st_1": {
+    title: "Mr. Wu",
+    owner: "N/A",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "N/A",
+    story: `Mr. Wu moved to Beach Street in January, 2014. In the spring of the following year, the landlord told Mr. Wu and his roommate to move out. The only reason the landlord stated was that she wants to make repairs to the apartment. Mr. Wu could benefit from a Just Cause Eviction ordinance. `,
+    Latitude: 42.351311,
+    Longitude: -71.059885,
+    images: [
+      Images.images.beach61
+    ],
+  },
+  "tremont_st_1": {
+    title: "Ms. Ye and Family",
+    owner: "Tremont Development LLC 615",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "N/A",
+    story: `Ms. Ye and her family had to move out of their Shawmut Ave. apartment building when it was sold in 2006. They then moved into an apartment on Tremont Street. In the decade that Ms. Ye's family has lived on Tremont, their rent has risen by over 45%.
 
+    Three other Chinese families lived in the building. In April of 2015, the building was sold for $2.86 million to Tremont Development LLC 615. The principal agent for the LLC is Sean Kennedy who also owns Kennedy Design and Building LLC, which specializes in converting apartments to luxury condos. 
+    
+    As soon as 615 Tremont Street LLC bought the building, they started clearing out the building of the former residents. By the end of summer 2015, Ms. Ye and her family were the only ones left in the building because they have no place to move to. They continue to be the only ones left, and it's uncertain where the family will end up.`,
+    Latitude: 42.351311,
+    Longitude: -71.059885,
+    images: [
+      Images.images.beach61
+    ],
+  },
+  "harvard_ave_1": {
+    title: "Arnie",
+    owner: "N/A",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "50%",
+    video: "https://www.facebook.com/bostondisplacement/videos/186700635039613/",
+    story: `About a year ago, Arnie received a 50% rent increase while living in a studio apartment near the Hyde Park train station. Arnie couldn't afford it and moved into a homeless shelter. Arnie's been homeless for about a year, and began taking drugs as a way of coping with the challenges of homelessness. Sober for four months now, Arnie still laments the inaccessibity of housing while rents rise throughout Boston.`,
+    Latitude: 42.259052,  
+    Longitude: -71.122347,
+    images: [],
+  },
+  "oakwood_st_1": {
+    title: "Christina",
+    owner: "Fannie Mae",
+    type: "story",
+    center: [42.37, -71.03],
+    rent: "N/A",
+    story: `Christina grew up in Roxbury and later moved into her family home in Dorchester. When Christina's aunt passed away, the home fell into foreclosure. Fannie Mae took control of the property and gave Christina a no-fault eviction notice. "Fannie Mae's playing hardball," said Christina. "If Just Cause eviction were in place, I'd have more time to move, or to see if negotiations went through...The home where I lived was a place of love, was a place of refuge."`,
+    Latitude: 42.288452, 
+    Longitude: -71.073895,
+    images: [
+      Images.images.beach61
+    ],
+  },
 }
 
 export default eastBostonStories;
