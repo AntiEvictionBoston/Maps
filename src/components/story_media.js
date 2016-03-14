d1496a78221f44d2180ddb2a9c64b1f20dfc1658
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-13 23:06:31 -0400

    add an expansion by paragraph to the story

--- src/components/story_media.js
+++ src/components/story_media.js
@@ -6,18 +6,28 @@ class StoryMedia extends React.Component {
   render () {
     return (
       <div id="story-media">
-        {this.renderImages()}
-        {this.renderVideo()}
+        <div id="images">
+          {this.renderImages()}
+        </div>
+        <div id="video">
+          {this.renderVideo()}
+        </div>
+        <div id="story">
+          {this.renderStory()}
+        </div>
       </div>
     );
   }
 
   componentWillMount () {
-    this.setState({ showAllImages: false });
+    this.setState({
+      showAllImages: false,
+      showAllStory: false,
+    });
   }
 
   renderImages () {
-    var images = [];
+    let images = [];
     this.props.story.images.forEach((image, index) => (
       images.push (
         <div
@@ -28,22 +38,95 @@ class StoryMedia extends React.Component {
       )
     ));
     if ( images.length > 1 ) {
-      images.push(this.showMoreButton(images.length + 1));
+      images.push(this.showMoreImagesButton(images.length + 1));
     }
     return images;
   }
 
-  showMoreButton (index) {
+  showMoreImagesButton (index) {
     return (
-      <button key={index} onClick={this.toggleShowState}>{this.showButtonText()}</button>
+      <div className="button-container">
+        <button key={index} onClick={this.toggleShowImages} className="expand-images-button">
+          {this.showButtonText()}
+        </button>
+      </div>
     );
   }
-
-  toggleShowState = () => this.setState({ showAllImages: !this.state.showAllImages });
-  showButtonText = () => this.state.showAllImages ? "collapse" : "expand";
+  toggleShowImages = () => this.setState({ showAllImages: !this.state.showAllImages });
+  showButtonText () {
+    if ( this.state.showAllImages ){
+      return (
+        <div>
+          <i className="fa fa-arrow-up"></i>
+          {" collapse"}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <i className="fa fa-arrow-down"></i>
+          {" expand"}
+       </div>
+      );
+    }
+  }
 
   setHidingState (index) {
     if ( this.state.showAllImages ) {
+      return "";
+    } else {
+      return index === 0 ? "" : "hidden";
+    }
+  }
+
+  renderStory () {
+    let paragraphs = [];
+    this.props.story.story.split("\n").forEach((paragraph, index) => {
+      paragraphs.push (
+        <p
+          key={index}
+          className={"story-paragraph " + this.setStoryHidingState(index)}>
+          {paragraph}
+        </p>
+      );
+    });
+    if ( paragraphs.length > 1 ) {
+      paragraphs.push(this.showStoryButton(paragraphs.length + 1));
+    }
+    return paragraphs;
+  }
+
+  showStoryButton (index) {
+    return (
+      <div className="button-container">
+        <button key={index} onClick={this.toggleShowStory} className="expand-story-button">
+          {this.showStoryText()}
+        </button>
+      </div>
+    );
+  }
+  toggleShowStory = () => this.setState({ showAllStory: !this.state.showAllStory });
+  showStoryText () {
+    if ( this.state.showAllStory ) {
+      return (
+        <div>
+          <i className="fa fa-arrow-up"></i>
+          {" collapse"}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <i className="fa fa-arrow-down"></i>
+          {" expand"}
+       </div>
+      );
+    }
+  }
+
+
+  setStoryHidingState (index) {
+    if ( this.state.showAllStory ) {
       return "";
     } else {
       return index === 0 ? "" : "hidden";
