9176a0364ea2b514621fba0ec4e643c86e9720a7
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-09 13:07:28 -0400

    tweak express app to use the build dir

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080
 const app = express()
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/build'));
 
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'index.html'))
+  response.sendFile(path.resolve(__dirname + '/build', 'index.html'))
 })
+
+const port = process.env.PORT || 8080
+app.listen(port);
+console.log(`server started on port ${port}`);
